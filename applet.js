657d53e91eea2c2a84a2e2183cb9149869016652
Stephen Collins <scrivera64@gmail.com>
2014-01-03 12:02:22 -0700

    RC

--- applet.js
+++ applet.js
@@ -4,62 +4,95 @@ const St = imports.gi.St;
 const Applet = imports.ui.applet;
 const PopupMenu = imports.ui.popupMenu;
 const Settings = imports.ui.settings;
+const ScreenSaver = imports.misc.screenSaver;
 const Util = imports.misc.util;
-//const DBus = imports.dbus;
 const Lang = imports.lang;
 
-const ENTRIES = [
-    { id: "logOff", title: "Log Off" },
-    { id: "restart", title: "Restart" },
-    { id: "shutDown", title: "Shut Down" },
-    { id: "hibernate", title: "Hibernate" },
-    { id: "suspend", title: "Suspend" }
-];
 
+let button_path, menu_item_icon_size, use_symbolic_icons;
+let has_console_kit, has_upower, has_systemd, session_manager;
 
-let menu_item_icon_size, menu_item_icon_type, has_console_kit, has_upower;
-
-
-CommandDispatcher = {
+let CommandDispatcher = {
     shutDown: function() {
         if ( has_console_kit ) Util.spawnCommandLine("dbus-send --system --print-reply --system --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop");
         else if ( has_systemd ) Util.spawnCommandLine("systemctl poweroff");
     },
     
     restart: function() {
-        Util.spawnCommandLine("dbus-send --system --print-reply --system --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart");
+        if ( has_console_kit ) Util.spawnCommandLine("dbus-send --system --print-reply --system --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart");
+        else if ( has_systemd ) Util.spawnCommandLine("systemctl reboot");
+    },
+    
+    hibernate: function() {
+        if ( has_upower ) Util.spawnCommandLine("dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Hibernate");
+        else if ( has_systemd ) Util.spawnCommandLine("systemctl hibernate");
+    },
+    
+    suspend: function() {
+        if ( has_upower ) Util.spawnCommandLine("dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend");
+        else if ( has_systemd ) Util.spawnCommandLine("systemctl suspend");
+    },
+    
+    sleep: function() {
+        if ( has_systemd ) Util.spawnCommandLine("systemctl hybrid-sleep");
     },
     
     logOff: function() {
         Util.spawnCommandLine("dbus-send --session --type=method_call --print-reply --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout uint32:1");
     },
     
-    suspend: function() {
-        Util.spawnCommandLine("dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend");
+    uSwitch: function() {
+        switch ( session_manager ) {
+            case 0: //lightdm
+                Util.spawnCommandLine("cinnamon-screensaver-command --lock");
+                Util.spawnCommandLine("dm-tool switch-to-greeter");
+                break;
+            case 1: //mdm
+                Util.spawnCommandLine("mdmflexiserver");
+                break;
+            case 2: //gdm
+                Util.spawnCommandLine("cinnamon-screensaver-command --lock");
+                Util.spawnCommandLine("gdmflexiserver");
+                break;
+        }
     },
     
-    hibernate: function() {
-        Util.spawnCommandLine("dbus-send --print-reply --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Hibernate");
+    guest: function() {
+        if ( session_manager == 0 ) {
+            Util.spawnCommandLine("cinnamon-screensaver-command --lock");
+            Util.spawnCommandLine("dm-tool switch-to-guest");
+        }
+    },
+    
+    lock: function() {
+        this._screenSaverProxy = new ScreenSaver.ScreenSaverProxy();
+        let screensaver_settings = new Gio.Settings({ schema: "org.cinnamon.screensaver" });
+        let screensaver_dialog = Gio.file_new_for_path("/usr/bin/cinnamon-screensaver-command");
+        if ( screensaver_dialog.query_exists(null) ) {
+            if ( screensaver_settings.get_boolean("ask-for-away-message") ) Util.spawnCommandLine("cinnamon-screensaver-lock-dialog");
+            else Util.spawnCommandLine("cinnamon-screensaver-command --lock");
+        }
+        else this._screenSaverProxy.LockRemote();
     }
 }
 
 
-function MenuItem(info, params) {
-    this._init(info, params);
+function MenuItem(menu, info, params) {
+    this._init(menu, info, params);
 }
 
 MenuItem.prototype = {
     __proto__: PopupMenu.PopupBaseMenuItem.prototype,
     
-    _init: function(info, params) {
+    _init: function(menu, info, params) {
         try {
             
+            this.menu = menu;
             this.id = info.id;
             
             PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
             
-            //this.command = command;
-            //this.addActor(this.getIcon());
+            this.addActor(this.getIcon());
             
             let label = new St.Label({ text: info.title });
             this.addActor(label);
@@ -70,21 +103,31 @@ MenuItem.prototype = {
         }
     },
     
-    getIcon: function(iconString) {
-        if ( iconString.split("/").length > 1 ) {
-            return null;
+    getIcon: function() {
+        let iconType, iconPath;
+        if ( use_symbolic_icons ) {
+            iconPath = button_path + this.id + "-symbolic.svg";
+            iconType = St.IconType.SYMBOLIC;
         }
         else {
-            return new St.Icon({ icon_name: iconString, icon_size: menu_item_icon_size, icon_type: menu_item_icon_type });
+            iconPath = button_path + this.id + ".svg";
+            iconType = St.IconType.FULLCOLOR;
         }
+        
+        let file = Gio.file_new_for_path(iconPath);
+        let gicon = new Gio.FileIcon({ file: file });
+        let icon = new St.Icon({ gicon: gicon, icon_size: menu_item_icon_size, icon_type: iconType });
+        
+        return icon;
     },
     
     activate: function() {
-try {
-    CommandDispatcher[this.id]();
-} catch (e) {
-    global.logError(e);
-}
+        try {
+            this.menu.close();
+            CommandDispatcher[this.id]();
+        } catch (e) {
+            global.logError(e);
+        }
     }
 }
 
@@ -102,8 +145,11 @@ MyApplet.prototype = {
             this.metadata = metadata;
             this.instanceId = instanceId;
             this.orientation = orientation;
+            button_path = metadata.path + "/buttons/";
             Applet.TextIconApplet.prototype._init.call(this, this.orientation, panel_height);
             this.menuManager = new PopupMenu.PopupMenuManager(this);
+            this.menu = new Applet.AppletPopupMenu(this, this.orientation);
+            this.menuManager.addMenu(this.menu);
             
             this._checkSession();
             
@@ -112,8 +158,8 @@ MyApplet.prototype = {
             this.buildMenu();
             
             //set up panel
-            this._set_panel_icon();
-            this._set_panel_text();
+            this.setPanelIcon();
+            this.setPanelText();
             this.set_applet_tooltip(_("Session"));
             
         } catch(e) {
@@ -139,53 +185,110 @@ MyApplet.prototype = {
         if ( String(output).split("\n").length > 2 ) has_systemd = true;
         
         //check session manager
-        
+        if ( GLib.getenv("XDG_SEAT_PATH") ) session_manager = 0;
+        else if ( GLib.file_test("/usr/bin/mdmflexiserver", GLib.FileTest.EXISTS) ) session_manager = 1;
+        else if (GLib.file_test("/usr/bin/gdmflexiserver", GLib.FileTest.EXISTS)) session_manager = 2;
     },
     
     _bindSettings: function() {
         this.settings = new Settings.AppletSettings(this, this.metadata["uuid"], this.instanceId);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "panelIcon", "panelIcon", this._set_panel_icon);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "panelText", "panelText", this._set_panel_text);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "panelIcon", "panelIcon", this.setPanelIcon);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "symbolicPanelIcon", "symbolicPanelIcon", this.setPanelIcon);
+        this.settings.bindProperty(Settings.BindingDirection.IN, "panelText", "panelText", this.setPanelText);
         this.settings.bindProperty(Settings.BindingDirection.IN, "iconSize", "iconSize", this.buildMenu);
-        this.settings.bindProperty(Settings.BindingDirection.IN, "symbolic", "symbolic", this.buildMenu);
-        //for ( let i = 0; i < ENTRIES.length; i++ ) {
-        //    this.settings.bindProperty(Settings.BindingDirection.IN, ENTRIES[i].id+"Show", ENTRIES[i].id+"Show", this.buildMenu);
-        //    this.settings.bindProperty(Settings.BindingDirection.IN, ENTRIES[i].id+"Icon", ENTRIES[i].id+"Icon", this.buildMenu);
-        //    this.settings.bindProperty(Settings.BindingDirection.IN, ENTRIES[i].id+"Command", ENTRIES[i].id+"Command", this.buildMenu);
-        //}
-        //this._setKeybinding();
+        this.settings.bindProperty(Settings.BindingDirection.IN, "symbolicMenuIcons", "symbolicMenuIcons", this.buildMenu);
     },
     
     buildMenu: function() {
         try {
             
-            if ( this.menu ) this.menu.destroy();
+            this.menu.removeAll();
             
             menu_item_icon_size = this.iconSize;
-            if ( this.symbolic ) menu_item_icon_type = St.IconType.SYMBOLIC;
-            else menu_item_icon_type = St.IconType.FULLCOLOR;
+            use_symbolic_icons = this.symbolicMenuIcons;
             
-            this.menu = new Applet.AppletPopupMenu(this, this.orientation);
-            this.menuManager.addMenu(this.menu);
+            //lock
+            let lock = new MenuItem(this.menu, { id: "lock", title: "Lock Screen" });
+            this.menu.addMenuItem(lock);
             
-            for ( let i = 0; i < ENTRIES.length; i++ ) {
-                let menuItem = new MenuItem(ENTRIES[i]);
-                this.menu.addMenuItem(menuItem);
+            //switch user
+            let uSwitch = new MenuItem(this.menu, { id: "uSwitch", title: "Switch User" });
+            this.menu.addMenuItem(uSwitch);
+            
+            //guest
+            if ( session_manager == 0 ) {
+                let guest = new MenuItem(this.menu, { id: "guest", title: "Guest Session" });
+                this.menu.addMenuItem(guest);
             }
+            
+            //log off
+            let logOff = new MenuItem(this.menu, { id: "logOff", title: "Log Off" });
+            this.menu.addMenuItem(logOff);
+            
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            
+            //suspend
+            let suspend = new MenuItem(this.menu, { id: "suspend", title: "Suspend" });
+            this.menu.addMenuItem(suspend);
+            
+            //sleep
+            if ( has_systemd ) {
+                let sleep = new MenuItem(this.menu, { id: "sleep", title: "Sleep" });
+                this.menu.addMenuItem(sleep);
+            }
+            
+            //hibernate
+            let hibernate = new MenuItem(this.menu, { id: "hibernate", title: "Hibernate" });
+            this.menu.addMenuItem(hibernate);
+            
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            
+            //restart
+            let restart = new MenuItem(this.menu, { id: "restart", title: "Restart" });
+            this.menu.addMenuItem(restart);
+            
+            //shut down
+            let shutDown = new MenuItem(this.menu, { id: "shutDown", title: "Shut Down" });
+            this.menu.addMenuItem(shutDown);
             
         } catch(e) {
             global.logError(e);
         }
     },
     
-    _set_panel_icon: function() {
-        if ( this.panelIcon.split("/").length > 1 ) this.set_applet_icon_path(this.panelIcon);
-        else this.set_applet_icon_symbolic_name(this.panelIcon);
+    setPanelIcon: function() {
+        if ( this.panelIcon.split("/").length > 1 ) {
+            if ( this.symbolicPanelIcon && this.panelIcon.search("-symbolic.svg") > 0 ) this.set_applet_icon_symbolic_path(this.panelIcon);
+            else this.set_applet_icon_path(this.panelIcon);
+        }
+        else {
+            if ( this.symbolicPanelIcon ) this.set_applet_icon_symbolic_name(this.panelIcon);
+            else this.set_applet_icon_name(this.panelIcon);
+        }
     },
     
-    _set_panel_text: function() {
+    setPanelText: function() {
         if ( this.panelText ) this.set_applet_label(this.panelText);
         else this.set_applet_label("");
+    },
+    
+    set_applet_icon_symbolic_path: function(icon_path) {
+        if (this._applet_icon_box.child) this._applet_icon_box.child.destroy();
+        
+        if (icon_path){
+            let file = Gio.file_new_for_path(icon_path);
+            let gicon = new Gio.FileIcon({ file: file });
+            if (this._scaleMode) {
+                let height = (this._panelHeight / DEFAULT_PANEL_HEIGHT) * PANEL_SYMBOLIC_ICON_DEFAULT_HEIGHT;
+                this._applet_icon = new St.Icon({gicon: gicon, icon_size: height,
+                                                icon_type: St.IconType.SYMBOLIC, reactive: true, track_hover: true, style_class: 'applet-icon' });
+            } else {
+                this._applet_icon = new St.Icon({gicon: gicon, icon_size: 22, icon_type: St.IconType.FULLCOLOR, reactive: true, track_hover: true, style_class: 'applet-icon' });
+            }
+            this._applet_icon_box.child = this._applet_icon;
+        }
+        this.__icon_type = -1;
+        this.__icon_name = icon_path;
     }
 }
 
